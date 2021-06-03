0b30eb54f1148e5cae6b9545e1481466e562c39f
kiteB <mmy789@sch.ac.kr>
2021-06-03 21:11:42 +0100

    [yeonju] Locker 파일 수정

--- Smart_Laundry/Locker.py
+++ Smart_Laundry/Locker.py
@@ -1,13 +1,16 @@
-import RPi.GPIO as GPIO
 import time
-import requests
-import Adafruit_CharLCD as LCD
 
-servo = 23
+import Adafruit_CharLCD as LCD
+import RPi.GPIO as GPIO
+import requests
+
+import pyzbar.pyzbar as pyzbar
+import cv2
+
 buzzer = 20
 led_red = 21
 led_green = 13
-locker = 27
+led_blue = 27
 door = 6
 
 lcd_rs = 26
@@ -23,74 +26,123 @@ lcd_rows = 2
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo, GPIO.OUT)
 GPIO.setup(buzzer, GPIO.OUT)
-GPIO.setup(led_red, GPIO.OUT)  # RED
-GPIO.setup(led_green, GPIO.OUT)  # GREEN
-GPIO.setup(locker, GPIO.OUT)
+GPIO.setup(led_red, GPIO.OUT)       # RED
+GPIO.setup(led_green, GPIO.OUT)     # GREEN
+GPIO.setup(led_blue, GPIO.OUT)      # BLUE
 GPIO.setup(door, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-p = GPIO.PWM(servo, 50)
 b = GPIO.PWM(buzzer, 262)
-p.start(0)
 
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 try:
     while True:
-        key = raw_input("Enter Y/N : ")
-        r = requests.post('http://127.0.0.1:8000/post/', data={'servo': key})
+        GPIO.output(led_green, 0)   # GREEN OFF
+        GPIO.output(led_red, 0)     # RED OFF
+        GPIO.output(led_blue, 0)    # BLUE OFF
 
-        if key.upper() == 'Y':
+        # QR Code 
+        cap = cv2.VideoCapture(0)
+        a = ''
+        i = 0
+
+        while cap.isOpened():
+            ret, img = cap.read()
+
+            if not ret:
+                continue
+
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            decoded = pyzbar.decode(gray)
+
+            for d in decoded:
+                a = d.data
+
+            if len(a) >= 1:
+                break
+            cv2.imshow('img', img)
+            key = cv2.waitKey(1)
+
+        cap.release()
+        cv2.destroyAllWindows()
+        print(a)
+
+        # Transferring data to server
+        data = {'code': a}    
+        r = requests.post('http://127.0.0.1:8000/post/', data=data)        
+
+        if GPIO.input(door):        # if door open
+            lcd.message("Door is open")
+            print("Door is open")
+            time.sleep(2.0)
+            lcd.clear()
+            b.start(50.0)
+            time.sleep(1.0)
+            b.stop()
+
+        else:                       # if door close
+            lcd.message("Door is close")
+            print("Door is close")
+            time.sleep(2.0)
+            lcd.clear()
+
+        # Correct
+        if len(a) >= 1:
             print(r.status_code)
             print("Welcome")
-            lcd.message("Welcome!")  # lcd 'welcome' print
+            
+            lcd.message("Welcome!")     # lcd 'welcome'   
             time.sleep(2.0)
-            lcd.clear()  # display clear
-            GPIO.output(led_green, 1)  # GREEN ON
-            GPIO.output(led_red, 0)  # RED OFF
-            GPIO.output(locker, 1)  # LOCKER OPEN
-            p.ChangeDutyCycle(9.5)
-            time.sleep(0.5)
-            p.ChangeDutyCycle(2.5)
-            time.sleep(0.5)
-            GPIO.output(locker, 0)  # LOCKER CLOSE AGAIN
+            lcd.clear()                 # display clear
+            GPIO.output(led_green, 1)   # GREEN ON
+            GPIO.output(led_red, 0)     # RED OFF
+            GPIO.output(led_blue, 0)    # BLUE OFF
 
-            if GPIO.input(door):  # if door open
+            if GPIO.input(door):        # if door open
                 lcd.message("Door is open")
                 print("Door is open")
+                GPIO.output(led_green, 0)   # GREEN OFF
+                GPIO.output(led_red, 1)     # RED ON
+                GPIO.output(led_blue, 0)    # RED OFF
+                
                 time.sleep(2.0)
                 lcd.clear()
                 b.start(50.0)
                 time.sleep(1.0)
                 b.stop()
-                p.ChangeDutyCycle(2.5)
-                time.sleep(0.5)
 
-            else:                 # if door close
+            else:                       # if door close
                 lcd.message("Door is close")
                 print("Door is close")
                 time.sleep(2.0)
                 lcd.clear()
-                
-        if key.upper() == 'N':
+        
+        # Incorrect
+        else:
             print("Try again!")
-            lcd.message("Try again!")  # lcd 'Try again' print
+            lcd.message("Try again!")   # lcd 'Try again' print
             time.sleep(2.0)
-            lcd.clear()  # display clear
-            GPIO.output(led_green, 0)  # GREEN OFF
-            GPIO.output(led_red, 1)  # RED ON
-            GPIO.output(locker, 0)  # LOCKER CLOSE
+            lcd.clear()                 # display clear
+            GPIO.output(led_green, 0)   # GREEN OFF
+            GPIO.output(led_red, 1)     # RED ON
+            GPIO.output(led_blue, 0)    # RED OFF
+
             b.start(50.0)
             time.sleep(1.0)
             b.stop()
-            p.ChangeDutyCycle(0)
-            time.sleep(0)
 
-        p.ChangeDutyCycle(0)
         time.sleep(0)
 
+        GPIO.output(led_green, 0)   # GREEN OFF
+        GPIO.output(led_red, 0)     # RED OFF
+        GPIO.output(led_blue, 0)    # BLUE OFF
+        time.sleep(0.2)
+
+
 except KeyboardInterrupt:
-    p.stop()
+    GPIO.output(led_green, 0)   # GREEN OFF
+    GPIO.output(led_red, 0)     # RED OFF
+    GPIO.output(led_blue, 0)    # BLUE OFF
 
 GPIO.cleanup()

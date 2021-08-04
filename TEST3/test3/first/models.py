0b9441966f3e908406a617654b3bab7af40b9f62
kiteB <69155170+kiteB@users.noreply.github.com>
2021-08-04 13:35:32 +0900

    Delete TEST3/test3 directory

--- TEST3/test3/first/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Sensor(models.Model):
-    numbers = models.CharField(max_length=500, null=True)
-    servo = models.CharField(max_length=500, null=True)
-    code = models.CharField(max_length=500, null=True)
-    # status = models.CharField(max_length=500, null=True)
--- TEST3/test3/first/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Sensor
-
-
-class SensorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sensor
-        fields = '__all__'
